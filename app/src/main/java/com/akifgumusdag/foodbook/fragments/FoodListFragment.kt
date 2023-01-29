c893a7c31b20172d93e778b6d9948337693c2691
akif_gumusdag <oakifgumusdag@gmail.com>
2023-01-29 23:21:32 +0300

    **FoodBook**
    - Fragments added

---
+++ app/src/main/java/com/akifgumusdag/foodbook/fragments/FoodListFragment.kt
@@ -0,0 +1,19 @@
+package com.akifgumusdag.foodbook.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akifgumusdag.foodbook.R
+
+class FoodListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_list, container, false)
+    }
+}
